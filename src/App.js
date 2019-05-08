8411714ce9cc6934c423a40db5a9a9649e76daaf
Stu Slack <sslack@apple.com>
2019-05-07 19:43:52 -0700

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AWS = require('aws-sdk');
+const AWSIoTData = require('aws-iot-device-sdk');
+
+let awsConfig = {
+  identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+  mqttEndpoint: `${process.env.REACT_APP_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com`,
+  region: process.env.REACT_APP_REGION,
+  clientId: process.env.clientId,
+  userPoolId: process.env.REACT_APP_USER_POOL_ID
+};
+
+const mqttClient = AWSIoTData.device({
+  region: awsConfig.region,
+  host: awsConfig.mqttEndpoint,
+  clientId: awsConfig.clientId,
+  protocol: 'wss',
+  maximumReconnectTimeMs: 8000,
+  debug: false,
+  accessKeyId: '',
+  secretKey: '',
+  sessionToken: ''
+});
+
+AWS.config.region = awsConfig.region;
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: awsConfig.identityPoolId
+});
+
+AWS.config.credentials.get((err) => {
+    if (err) {
+        console.log(AWS.config.credentials);
+        throw err;
+    } else {
+        mqttClient.updateWebSocketCredentials(
+            AWS.config.credentials.accessKeyId,
+            AWS.config.credentials.secretAccessKey,
+            AWS.config.credentials.sessionToken
+        );
+    }
+});
+
+mqttClient.on('connect', () => {
+  console.log('mqttClient connected')
+  mqttClient.subscribe('real-time-weather')
+});
+
+mqttClient.on('error', (err) => {
+  console.log('mqttClient error:', err)
+});
+
+mqttClient.on('message', (topic, payload) => {
+  const msg = JSON.parse(payload.toString());
+  console.log('mqttClient message: ', msg);
+});
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Realtime Weather</h1>
+                <p>Check the console..</p>
+            </div>
+        );
+    }
 }
 
 export default App;
